722090b8f101c3fb6eea8cfead6e15e2f691051c
sailorbl <72577047+sailorbl@users.noreply.github.com>
2020-12-05 10:30:16 +0200

    Added lines input name and surname

--- add.py
+++ add.py
@@ -2,3 +2,7 @@ print('I’m planning to learn git conflicts resolution')
 
 nickname = input('Please enter your nickname')
 print('Hi, {}!'.format(nickname))
+
+name = input('Please enter your name:')
+surname = input('Please enter your surname:')
+print('Hi, {} {}!'.format(name, surname))
